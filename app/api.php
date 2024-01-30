5f387dcf6dcf6dc4da1e5b39e03d12c97025670b
Manuel Chavarria <manuelchavarria337@gmail.com>
2024-01-30 15:40:50 -0600

    Clase 55: Conectandose a una base de datos MySQL y PHP

--- app/api.php
+++ app/api.php
@@ -1,5 +1,20 @@
 <?php
 
+// datos de la base de datos
+$servidor="localhost";
+$usuario="root";
+$password="";
+$basededatos="app";
+
+// conexion a la base de datos
+$conexion = new mysqli($servidor, $usuario, $password, $basededatos);
+
+if(!$conexion->connect_error){
+    echo "Conexión establecida";
+}else{
+    print_r($conexion->connect_error);
+}
+
 if($_POST){
     echo "se se agrego la tarea: ".$_POST["tarea"];
 }
