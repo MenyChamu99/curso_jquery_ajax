f81a8ec500483f24f6f9264575968dedf46444c6
Manuel Chavarria <manuelchavarria337@gmail.com>
2024-02-01 22:44:34 -0600

    Clase 58: Lectura de datos de la api php pt2

--- app/api.php
+++ app/api.php
@@ -12,25 +12,30 @@ $conexion = new mysqli($servidor, $usuario, $password, $basededatos);
 if($conexion->connect_error){
     print_r($conexion->connect_error);
 }
+$accion = $_POST["accion"];
 
 //recepciona las tareas
-if($_POST){
+if($accion == "agregar"){
     echo "se se agrego la tarea: ".$_POST["tarea"];
     $tarea=$_POST["tarea"];
     $sql="INSERT INTO tareas (id,tarea,completado) VALUES(NULL, '$tarea', '0');";
     $conexion->query($sql);
 }
 
-// mostrar las tareas de la base datos
-$sql = "SELECT * FROM tareas";
-$resultado = $conexion->query($sql); // aqui se corre la consulta a la base de datos para traer las tareas que se tienen en esta
+if($accion == "leer"){
+    // mostrar las tareas de la base datos
+    $sql = "SELECT * FROM tareas";
+    $resultado = $conexion->query($sql); // aqui se corre la consulta a la base de datos para traer las tareas que se tienen en esta
 
-if($resultado->num_rows>0){ // Si eñ numero de renglones es mayor que 0 quiere decir que si existen registros
-    $tareas = array();
-    while($fila = $resultado->fetch_assoc()){ // aqui se guardan los registros por cada columna de la tabla gracias al metodo fetch_assoc()
-        $tareas[] = $fila;
+    if($resultado->num_rows>0){ // Si eñ numero de renglones es mayor que 0 quiere decir que si existen registros
+        $tareas = array();
+        while($fila = $resultado->fetch_assoc()){ // aqui se guardan los registros por cada columna de la tabla gracias al metodo fetch_assoc()
+            $tareas[] = $fila;
+        }
+        echo json_encode(array('success'=>true, 'datos'=>$tareas));
     }
-    echo json_encode(array('success'=>true, 'datos'=>$tareas));
 }
+
+
 
 ?>
